978f8a59c5c3d8c3c724d22a637efe3e28c09140
kishlay kumar <kishlay.business@gmail.com>
2024-03-24 18:11:17 +0530

    fix all register controller

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -16,13 +16,13 @@
     // return res
 
     const {fullName , email, username, password } = req.body
-    console.log("email", email);
+    //console.log("email", email);
 
     if([fullName, email, username, password].some((field) => field?.trim() === "")){
-      throw new ApiErroe(400, "All fields are required")
+      throw new ApiError(400, "All fields are required")
     }
 
-    const existedUser = user.findOne({
+    const existedUser = await User.findOne({
       $or: [{ username },{ email }]
     })
 
@@ -30,18 +30,25 @@
       throw new ApiError(409, "User with email or username already exists")
     }
 
+    //console.log(req.files);
+
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.file?.coverImage[0]?.path;
+    //const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
+      coverImageLocalPath = req.files.coverImage[0].path
+    }
 
     if (!avatarLocalPath) {
-      throw new ApiErroe(400, "Avatar file is required")
+      throw new ApiError(400, "Avatar file is required")
     }
 
     const avatar = await uploadOnCloudinary(avatarLocalPath)
     const coverImage = await uploadOnCloudinary(coverImageLocalPath)
 
     if(!avatar) {
-      throw new ApiErroe(400, "Avatar file is required")
+      throw new ApiError(400, "Avatar file is required")
     }
 
      const user =  await User.create({
@@ -57,7 +64,7 @@
     const createdUser = await User.findById(user._id).select("-password -refreshToken")
 
     if(!createdUser){
-      throw new ApiErroe(500, "Something went wrong while registering the user")
+      throw new ApiError(500, "Something went wrong while registering the user")
     }
 
     return res.status(201).json(
